b6f11c447b977eafd782dbdccb5aa13481328365
XBean1024 <596928539@qq.com>
2019-01-03 14:28:17 +0800

    css

---
+++ web/js/user.js
@@ -0,0 +1,27 @@
+/*
+* 用户登录或注册
+* */
+var ACCOUNT = "account";//账号
+var PASSWORD = "password";//密码
+function login() {
+    alert("登录");
+
+    //隐藏注册表单
+    document.home_login.style.display = 'block';
+    document.home_register.style.display = 'none';
+    var account = document.getElementById(ACCOUNT).value;
+    var password = document.getElementById(PASSWORD).value;
+    alert(account);
+    alert(password);
+
+}
+
+function register() {
+    alert("注册");
+    //获取登录表单
+    //获取账号控件
+    document.home_login.style.display = 'none';
+    document.home_register.style.display = 'block';
+    var account = document.getElementById('account_reg');
+    alert(account.value);
+}
