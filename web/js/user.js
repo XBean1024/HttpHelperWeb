2d5651b89997fd3f1902bf6e5bfd67d6f71c6a75
XBean1024 <596928539@qq.com>
2019-03-14 14:43:48 +0800

    新增 再次输入密码编辑框

--- web/js/user.js
+++ web/js/user.js
@@ -9,8 +9,8 @@ function login() {
     //隐藏注册表单
     document.home_login.style.display = 'block';
     document.home_register.style.display = 'none';
-    var account = document.getElementById(ACCOUNT).value;
-    var password = document.getElementById(PASSWORD).value;
+    const account = document.getElementById(ACCOUNT).value;
+    const password = document.getElementById(PASSWORD).value;
     alert(account);
     alert(password);
 
@@ -22,6 +22,16 @@ function register() {
     //获取账号控件
     document.home_login.style.display = 'none';
     document.home_register.style.display = 'block';
-    var account = document.getElementById('account_reg');
+    const account = document.getElementById('account_reg');
+    alert(account.value);
+}
+
+function to_register() {
+    alert("去注册");
+    //获取登录表单
+    //获取账号控件
+    document.home_login.style.display = 'none';
+    document.home_register.style.display = 'block';
+    const account = document.getElementById('account_reg');
     alert(account.value);
 }
